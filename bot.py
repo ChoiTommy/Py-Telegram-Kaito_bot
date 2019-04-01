795acbdc5f2046ef3c00d76dbd167d0a284f4dfb
Kaito <tommychoiht@gmail.com>
2019-04-01 20:28:06 +0800

    Handles replying non text messages

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 #https://youtu.be/jhFsFZXZbu4
+#https://github.com/eternnoir/pyTelegramBotAPI#message-handlers
 import telebot
-import time
 
 bot_token = ''
 
@@ -13,14 +13,23 @@ def send(message):
 def extract_arg(arg):
     return '+'.join(arg.split()[1:])
 
+def space_to_plus_sign(arg):
+    return '+'.join(arg.split())
+
 @bot.message_handler(commands=['google'])
 def google(message):
-    status = extract_arg(message.text)
-    bot.reply_to(message, 'https://www.giyf.com/' + status)
+    if message.reply_to_message is None:
+        status = extract_arg(message.text)
+        bot.reply_to(message, 'https://www.giyf.com/' + status)
+    elif not message.reply_to_message.text is None:
+        bot.reply_to(message.reply_to_message, 'https://www.giyf.com/' + space_to_plus_sign(message.reply_to_message.text))
 
 @bot.message_handler(commands=['wolframalpha', 'wa'])
 def wolframalpha(message):
-    status = extract_arg(message.text)
-    bot.reply_to(message, 'https://www.wolframalpha.com/input/?i=' + status)
+    if message.reply_to_message is None:
+        status = extract_arg(message.text)
+        bot.reply_to(message, 'https://www.wolframalpha.com/input/?i=' + status)
+    elif not message.reply_to_message.text is None:
+        bot.reply_to(message.reply_to_message, 'https://www.wolframalpha.com/input/?i=' + space_to_plus_sign(message.reply_to_message.text))
 
 bot.polling()
